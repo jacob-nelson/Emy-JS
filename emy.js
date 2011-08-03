78f446dc043b9c934258251a2134d1eb89fd7935
Jacob Nelson <iam@jnelson.in>
2011-08-03 10:32:28 -0400

    Emy.js Version 0.1.3 Adding new functionalities fill(), add(), plus()[alias of add()], subtract(), minus()[alias of subtract], negate()

--- emy.js
+++ emy.js
@@ -1,8 +1,9 @@
-/************************************************
-*	Author: Jacob Nelson						*
-*	Email Id: iam@jnelson.in					*
-*	Blog: http://jnelson.in						*
-************************************************/
+/********************************************************
+*	Author: Jacob Nelson								*
+*	Email Id: iam@jnelson.in							*
+*	Blog: http://jnelson.in								*
+*	Help: http://javascript.jnelson.in/category/emy-js	*
+********************************************************/
 Array.prototype.sizeOf = function(){ return this.length; }
 Array.prototype.min = function(){ return Math.min.apply({},this) }
 Array.prototype.max = function(){ return Math.max.apply({},this) }
@@ -15,6 +16,7 @@ Array.prototype.product = function(){
 	return prod;
 }
 Array.prototype.average = function(){ return this.sum() / this.length; }
+Array.prototype.avg = function(){ return this.average(); }
 Array.prototype.shuffle = function(){
 	for(var j, x, i = this.length; i; j = parseInt(Math.random() * i), x = this[--i], this[i] = this[j], this[j] = x);
 	return this;
@@ -45,6 +47,7 @@ Array.prototype.compare = function(array2){
 	}
 	return true;
 }
+Array.prototype.equal = function(array2){return this.compare(array2);}
 Array.prototype.unique = function(){
 	var uniq_array = new Array();
 	for(i=0;i<this.sizeOf();i++){
@@ -74,6 +77,7 @@ Array.prototype.flatten = function(){
 	var flattened = this.reduce(function(a,b) {
 		return a.concat(b);
 	}); 
+	
 	return flattened;
 }
 Array.prototype.restOf = function(){ return this.exclude(Array.prototype.slice.call(arguments)) }
@@ -127,4 +131,52 @@ Object.prototype.range = function(from, to, step){
 			 return -1;
 	}
 	return range_array;
+}
+Array.prototype.fill = function(fillWith, startIndex, num){
+	var fillArray = new Array();
+	fillArray = this;
+	count = fillArray.sizeOf();
+	if(typeof fillWith == 'undefined') return -1;
+	if(typeof startIndex == 'undefined') startIndex = 0;
+	else if(startIndex > count)startIndex = count;
+	if(typeof num == 'undefined') return -1;
+	
+	for(i=startIndex;i<(startIndex+num);i++){
+		fillArray[i] = fillWith;
+	}
+	return fillArray;
+}
+Array.prototype.add = function(array2){
+	var added = new Array();
+	iterate_count = this.sizeOf();
+	if(array2.sizeOf() > iterate_count){
+		iterate_count = array2.sizeOf();
+		this.fill(0, this.sizeOf(), iterate_count) ;
+	}
+	else
+		array2.fill(0, array2.sizeOf(), iterate_count);
+	for(i=0;i<iterate_count;i++){
+		added.push(this[i]+array2[i]);
+	}
+	return added;
+}
+Array.prototype.plus = function(array2){ return this.add(array2); }
+Array.prototype.subtract = function(array2){
+	var subtracted = new Array();
+	iterate_count = this.sizeOf();
+	if(array2.sizeOf() > iterate_count){
+		iterate_count = array2.sizeOf();
+		this.fill(0, this.sizeOf(), iterate_count) ;
+	}
+	else
+		array2.fill(0, array2.sizeOf(), iterate_count);
+	for(i=0;i<iterate_count;i++){
+		subtracted.push(this[i]-array2[i]);
+	}
+	return subtracted;
+}
+Array.prototype.minus = function(array2){ return this.subtract(array2); }
+Array.prototype.negate = function(){ 
+	var b = new Array(0);
+	return b.minus(this); 
 }
