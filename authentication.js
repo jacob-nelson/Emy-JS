f2b2f82117690da15a350d9c71a397b958856965
Jacob Nelson <jacobnelson79@gmail.com>
2018-03-12 15:28:06 +0530

    Update authentication.js

--- authentication.js
+++ authentication.js
@@ -1,10 +1,9 @@
 function parseCookies() {
   var body = document.getElementsByTagName('body')[0].innerHTML;
   var name = document.getElementById('nameValue').value
-  var auth = document.getElementById('session')
-  var dataSession = auth.getAttribute('session')
+  var auth = document.getElementById('session').innerHTML;
   console.log("name inside parseCookies ", name)
-  return dataSession;
+  return auth;
   //return "some return value";
 }
 /*
